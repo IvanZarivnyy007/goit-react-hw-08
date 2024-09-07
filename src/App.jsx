b949cb16a84c8e08b4933fcd13d6e5b5e90b8b11
Ivan Zarivnyy <144140704+IvanZarivnyy007@users.noreply.github.com>
2024-09-07 08:21:45 +0200

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,18 @@ const RegistrationPage = lazy(() => import('./pages/RegistrationPage'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
 const ContactsPage = lazy(() => import('./pages/ContactsPage'));
 
+import Layout from './components/Layout/Layout';
+
 function App() {
   return (
     <>
+      <Layout />
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegistrationPage />} />,
           <Route path="/login" element={<LoginPage />} />,
-          <Route path="/contacts" element={<ContactsPage />} />,{/* </Route> */}
+          <Route path="/contacts" element={<ContactsPage />} />,
         </Routes>
       </Suspense>
     </>
