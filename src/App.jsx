8d49a8a57b54fb8c6b8a3e5bcd596c050fa3da70
Ivan Zarivnyy <144140704+IvanZarivnyy007@users.noreply.github.com>
2024-09-07 08:37:11 +0200

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
+import Layout from './components/Layout/Layout';
+// import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 const HomePage = lazy(() => import('./pages/Home'));
 const RegistrationPage = lazy(() => import('./pages/RegistrationPage'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
 const ContactsPage = lazy(() => import('./pages/ContactsPage'));
-
-import Layout from './components/Layout/Layout';
 
 function App() {
   return (
--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './redux/contact/contactsSlice';
 import filtersReducer from './redux/filters/filtersSlice';
+import authReducer from './redux/auth/slice';
+
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     contacts: contactsReducer,
     filters: filtersReducer,
   },
